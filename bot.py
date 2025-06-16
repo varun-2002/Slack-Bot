426687f21a96f0bfd2455402e4f20a4500243aff
varun-2002 <87017563+varun-2002@users.noreply.github.com>
2025-06-16 16:24:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,18 +9,20 @@ env_path=Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
 app=Flask(__name__)
+BOT_ID = client.api_call("auth.test")['user_id']
 
 slack_event_adapter= SlackEventAdapter( os.environ['SIGNING_SECRET'], '/slack/events',app )
 
 @slack_event_adapter.on("message")
 def message(payload):
-    print(payload)
-    print("message")
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    if user_id!= BOT_ID:
+        client.chat_postMessage(channel="#test", text="Welcome to the Channel")
 
-@slack_event_adapter.on("message.channels")
-def message(payload):
-    print(payload)
-    print("channels")
+
 
 client=slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
