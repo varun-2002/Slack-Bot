5d73fd399e92c5a19b27b73d4c7d4a9bcff4e61c
varun-2002 <87017563+varun-2002@users.noreply.github.com>
2025-06-16 15:34:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,6 @@ client=slack.WebClient(token=os.environ['SLACK_TOKEN'])
 client.chat_postMessage(channel="#test", text="Welcome to the Channel")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
 
 
