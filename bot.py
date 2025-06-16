3ea9a34343868afd00e71c25b7bce63ea55ecfc7
varun-2002 <87017563+varun-2002@users.noreply.github.com>
2025-06-16 20:39:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,11 +20,25 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    app.logger.info("printing in the console")
-    if user_id!= BOT_ID:
-        client.chat_postMessage(channel="#test", text="Welcome to the Channel")
+    
+    if user_id != None and BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+
         client.chat_postMessage(channel="#test", text=text)
 
+@ app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+
+    client.chat_postMessage(
+        channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
 
 
 
